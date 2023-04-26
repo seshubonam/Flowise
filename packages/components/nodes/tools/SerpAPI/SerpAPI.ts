519d77e1403f27a3e4cae6c54b402438390d405d
seshubonam <seshu.bonam@gmail.com>
2023-04-27 04:03:22 +0530

    Update SerpAPI.ts

--- packages/components/nodes/tools/SerpAPI/SerpAPI.ts
+++ packages/components/nodes/tools/SerpAPI/SerpAPI.ts
@@ -1,38 +1,58 @@
-import { INode, INodeData, INodeParams } from '../../../src/Interface'
-import { getBaseClasses } from '../../../src/utils'
-import { SerpAPI } from 'langchain/tools'
+import { INode, INodeData, INodeParams } from '../../../src/Interface';
+import { getBaseClasses } from '../../../src/utils';
 
-class SerpAPI_Tools implements INode {
-    label: string
-    name: string
-    description: string
-    type: string
-    icon: string
-    category: string
-    baseClasses: string[]
-    inputs: INodeParams[]
-
-    constructor() {
-        this.label = 'Serp API'
-        this.name = 'serpAPI'
-        this.type = 'SerpAPI'
-        this.icon = 'serp.png'
-        this.category = 'Tools'
-        this.description = 'Wrapper around SerpAPI - a real-time API to access Google search results'
-        this.inputs = [
-            {
-                label: 'Serp Api Key',
-                name: 'apiKey',
-                type: 'password'
-            }
-        ]
-        this.baseClasses = [this.type, ...getBaseClasses(SerpAPI)]
-    }
-
-    async init(nodeData: INodeData): Promise<any> {
-        const apiKey = nodeData.inputs?.apiKey as string
-        return new SerpAPI(apiKey)
-    }
+export interface AIPluginToolParams {
+  name: string;
+  description: string;
+  apiSpec: string;
 }
 
-module.exports = { nodeClass: SerpAPI_Tools }
+export class OpenAPI_Tool implements INode, AIPluginToolParams {
+  private _name: string;
+  private _description: string;
+  apiSpec: string;
+
+  label: string;
+  type: string;
+  icon: string;
+  category: string;
+  inputs: INodeParams[];
+  baseClasses: string[];
+
+  get name() {
+    return this._name;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  constructor(params: AIPluginToolParams) {
+    this._name = params.name;
+    this._description = params.description;
+    this.apiSpec = params.apiSpec;
+
+    this.label = this._name;
+    this.type = 'OpenAPI';
+    this.icon = 'openapi.png';
+    this.category = 'Tools';
+    this.inputs = [
+      // Define any input parameters needed for the tool
+    ];
+    this.baseClasses = [this.type];
+  }
+
+  async init(nodeData: INodeData): Promise<any> {
+    // Implement the init method based on the OpenAPI specification
+  }
+}
+
+// Usage example:
+const openAPISpec = {}; // Load the OpenAPI specification from a file or URL
+const aiPluginToolParams = {
+  name: 'My API',
+  description: 'An example API',
+  apiSpec: JSON.stringify(openAPISpec),
+};
+
+module.exports = { nodeClass: new OpenAPI_Tool(aiPluginToolParams) };
